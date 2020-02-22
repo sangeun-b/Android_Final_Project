56ab895de95463049655afefec53a273c8762300
Hsing-I <hsing.sophie.gmail.com>
2020-02-21 19:28:54 -0500

    Create four activity

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Guardianb= (Button)findViewById(R.id.b1);
+        Button NasaDayb= (Button)findViewById(R.id.b2);
+        Button NasaEarthb= (Button)findViewById(R.id.b3);
+        Button BBCb= (Button)findViewById(R.id.b4);
+
+        Intent goToGuardian= new Intent(MainActivity.this, GuardianActivity.class);
+        Guardianb.setOnClickListener(click->startActivity(goToGuardian));
+
+        Intent goToNasaDayb= new Intent(MainActivity.this, NasaDayActivity.class);
+        Guardianb.setOnClickListener(click->startActivity(goToNasaDayb));
+
+        Intent goToNasaEarth= new Intent(MainActivity.this , NasaEarthActivity.class);
+        Guardianb.setOnClickListener(click->startActivity(goToNasaEarth));
+
+        Intent goToBBC= new Intent(MainActivity.this, BBCActivity.class);
+        Guardianb.setOnClickListener(click->startActivity(goToBBC));
+
+
+
+
+
+
+
     }
 }
