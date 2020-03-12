506544b20f7400b5dd5d2e46aa5e6918a5212142
Hsing-I <hsing.sophie.gmail.com>
2020-03-11 21:06:08 -0400

    Update NasaImage

---
+++ app/src/main/java/com/example/final_project/NasaImage.java
@@ -0,0 +1,88 @@
+package com.example.final_project;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class NasaImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nasa_image);
+
+        NasaDayImage fetchNasaImage = new NasaDayImage();
+        fetchNasaImage.execute("https://api.nasa.gov/planetary/apod?api_key=DgPLcIlnmN0Cwrzcg3e9NraFaYLIDI68Ysc6Zh3d&date=" + NasaDayActivity.DatePickerFragment.date);
+    }
+
+    class NasaDayImage extends AsyncTask<String, Integer, String> {
+
+        String date=null, url=null, hdUrl=null, title=null, ret=null;
+
+        public String doInBackground(String... args) {
+            //String queryurl = "https://api.nasa.gov/planetary/apod?api_key=DgPLcIlnmN0Cwrzcg3e9NraFaYLIDI68Ysc6Zh3d&date=" + NasaDayActivity.DatePickerFragment.date;
+            try {
+                URL link = new URL(args[0]);
+                HttpURLConnection connection = (HttpURLConnection) link.openConnection();
+                InputStream response = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line= null;
+
+                while ((line = reader.readLine()) != null) {sb.append(line + "\n"); }
+                String result = sb.toString();
+                JSONObject json = new JSONObject(result);
+
+                date= json.getString("date");
+                publishProgress(25);
+                //Log.i("NasaImage date", date);
+                url= json.getString("url");
+                publishProgress(50);
+                //Log.i("NasaImage url", url);
+                hdUrl= json.getString("hdurl");
+                publishProgress(75);
+                //Log.i("NasaImage hdurl", hdUrl);
+                title= json.getString("title");
+                publishProgress(100);
+                //Log.i("NasaImage title", title);
+                return "Done";
+            }
+            catch(MalformedURLException mfe){ ret = "Malformed URL exception"; }
+            catch(IOException ioe)          { ret = "IO Exception. Is the Wifi connected?";}
+            catch(JSONException je) { ret = "JSON exception."; }
+            return ret;
+        }
+
+        public void onProgressUpdate(Integer... args) {
+            super.onProgressUpdate(args);
+        }
+
+        public void onPostExecute(String fromDoInBackground) {
+            super.onPostExecute(fromDoInBackground);
+            TextView dateText = findViewById(R.id.dateTextView);
+            dateText.setText("DATE: " + date);
+            TextView titleText =findViewById(R.id.titleTextView);
+            titleText.setText("TITLE: " + title);
+            TextView urlText = findViewById(R.id.urlTextView);
+            urlText.setText("URL: " + url);
+            TextView hdUrlText =findViewById(R.id.hdurlTextView);
+            hdUrlText.setText("HDURL: "+ hdUrl);
+
+        }
+    }
+}
+
+
