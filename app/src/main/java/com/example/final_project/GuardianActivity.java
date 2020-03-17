7fb6f0402c66f22e19802c73dd629531424a5a96
qiwang661 <wang0690@algonquinlive.com>
2020-03-17 16:20:18 -0400

    guardian update1

--- app/src/main/java/com/example/final_project/GuardianActivity.java
+++ app/src/main/java/com/example/final_project/GuardianActivity.java
@@ -1,5 +1,6 @@
 package com.example.final_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class GuardianActivity extends AppCompatActivity {
 
@@ -16,6 +19,15 @@ public class GuardianActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guardian);
+
+        Button search =findViewById(R.id.search_button);
+        ImageButton favourite=findViewById(R.id.favouriteList);
+        
+
+
+
+
+
 
 
     }
