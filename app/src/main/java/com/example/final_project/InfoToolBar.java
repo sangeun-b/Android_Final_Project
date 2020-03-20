89c435e502a3175cfd348c038f2398c6c88d6dc8
Hsing-I <hsing.sophie.gmail.com>
2020-03-19 21:03:53 -0400

    update toolbar

--- app/src/main/java/com/example/final_project/InfoToolBar.java
+++ app/src/main/java/com/example/final_project/InfoToolBar.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
-
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class InfoToolBar extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class InfoToolBar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,14 @@ public class InfoToolBar extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbarinfo);
         setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -55,4 +67,52 @@ public class InfoToolBar extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog alert = builder.create();
+        builder.setTitle("How to use?");
+        switch(item.getItemId())
+        {
+            case R.id.item_guardian:
+                Intent guardianIntent= new Intent(InfoToolBar.this, GuardianActivity.class);
+                startActivity(guardianIntent);
+                break;
+            case R.id.item_nasaday:
+                Intent nasadayIntent= new Intent(InfoToolBar.this, NasaDayActivity.class);
+                startActivity(nasadayIntent);
+                break;
+            case R.id.item_nasaearth:
+                Intent nasaearthIntent= new Intent(InfoToolBar.this, NasaEarthActivity.class);
+                startActivity(nasaearthIntent);
+                break;
+            case R.id.item_bbc:
+                Intent bbcIntent= new Intent(InfoToolBar.this, BBCActivity.class);
+                startActivity(bbcIntent);
+                break;
+            case R.id.guide_guardian:
+                builder.setMessage("Enter a search word to find related articles from The Guardian newspaper.")
+                        .setPositiveButton("OK", (click, arg)-> alert.cancel())
+                        .show();
+                break;
+            case R.id.guide_nasaday:
+                builder.setMessage("Enter a day to view the image of thatt day pick by NASA. ")
+                        .setPositiveButton("OK", (click, arg)-> alert.cancel())
+                        .show();
+                break;
+            case R.id.guide_nasaearth:
+                builder.setMessage("Enter the latitude and longitude to view the satellite image of that position. ")
+                        .setPositiveButton("OK", (click, arg)-> alert.cancel())
+                        .show();
+                break;
+            case R.id.guide_bbc:
+                builder.setMessage("View the articles from BBC. ")
+                        .setPositiveButton("OK", (click, arg)-> alert.cancel())
+                        .show();
+                break;
+        }
+        DrawerLayout drawyer= (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawyer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
