f7d0b39faf5e04a515e7dc02543ac67e440b3f62
Hsing-I <hsing.sophie.gmail.com>
2020-04-06 00:06:24 -0400

    update

--- app/src/main/java/com/example/final_project/Image.java
+++ app/src/main/java/com/example/final_project/Image.java
@@ -1,7 +1,10 @@
 package com.example.final_project;
 
-import android.graphics.Bitmap;
-
+/**
+ * The image is a class that has no logic in it. It contains constructor, getter and setter for the five instance variables.
+ * @author Hsing-I Wang
+ * @version 1.0
+ */
 public class Image {
 
     private String date;
@@ -10,6 +13,9 @@ public class Image {
     private String hdurl;
     private long id;
 
+    /**
+     *five argument constructor
+     */
     public Image(String date, String title, String url, String hdurl, long id){
         this.date=date;
         this.title=title;
@@ -18,44 +24,72 @@ public class Image {
         this.id= id;
     }
 
+    /**
+     * getter for the date
+     */
     public String getDate() { return date; }
 
+    /**
+     * setter for the date
+     */
     public void setDate(String date) {
         this.date = date;
     }
 
+    /**
+     * getter for the title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * setter for the title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * getter for the url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * setter for the url
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * getter for the hdurl
+     */
     public String getHdurl() {
         return hdurl;
     }
 
+    /**
+     * setter for the hdurl
+     */
     public void setHdurl(String hdurl) {
         this.hdurl = hdurl;
     }
 
+    /**
+     * getter for the id
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * setter for the id
+     */
     public void setId(long id) {
         this.id = id;
     }
-
-
 
 }
